c844254296238a0dda72f5811bad09016ac769c2
syuhada-ramadhan <suhadahada18@gmail.com>
2021-02-16 17:48:46 +0700

    first commit

---
+++ js/convert.js
@@ -0,0 +1,8 @@
+// array hewan
+const hewan = ["Kambing", "Kelinci", "Kerbau", "Kucing", "Sapi"];
+
+const dataHewan = [];
+
+hewan.forEach((element) => dataHewan.push(element));
+
+console.log(dataHewan);
---
+++ js/script.js
@@ -0,0 +1,5 @@
+// array nomor
+const nomor = [1, 4, 5, 8, 10, 30, 50, 100];
+
+// melakukan pengulangan array nomor
+const ulangNomor = nomor.forEach((elemen) => console.log(elemen));
